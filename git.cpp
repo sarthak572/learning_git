be2b355192b1fa682ec14e380d081f19eb6a5fcc
sarthak7505045598 <sarthakbansal2023@gmail.com>
2024-10-11 01:15:16 +0530

    new commit

---
+++ git.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define fr(i,a,b) for (int i = a; i < b; i++)
+#define loop(n) for (int i = 0; i < n; i++)
+#define mod 1000000007
+#define inf (1ll<<60)
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+void precal() {
+    // TODO
+}
+
+void solve() {
+    // TODO
+    cout<<"hello world"; 
+    
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    loop(t) {
+        solve();
+    }
+}
